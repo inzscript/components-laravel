1c7fa01105467adebea36b83963319e9fcd944e5
Reggie <inzscript@gmail.com>
2025-01-05 16:38:25 -0800

    Add job factory models to auto generate db items

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 
 class Job extends Model {
+    use HasFactory, Notifiable;
+
     // Syntax for defining the table name that uses job_listings
     protected $table = 'job_listings';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'salary',
+    ];
 }
