86fc45349cb9b2180edef5d51fc9b2fc98341f37
Reggie <inzscript@gmail.com>
2025-01-06 18:30:00 -0800

    Simplify routes further using Resource, optional only and except

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::view('/', 'home');
-
-Route::controller(JobController::class)->group(function() {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
-
 Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class, [
+    // 'only' => ['index', 'create', 'show', 'store', 'edit', 'update', 'destroy']
+    // 'except' => ['edit']
+]);
